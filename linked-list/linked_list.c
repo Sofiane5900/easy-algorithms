13793b8e453db94ffaf4429ce1cd0cbf33e9547b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-27 12:40:21 +0200

    feat(linked-list): adding node struct

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -1,3 +1,13 @@
 //
 // Created by Sofiane on 26/04/2025.
 //
+
+// c'est une suite d'élement
+// Chaque element contient une réference (pointeur) a un autre élement
+
+int main() {
+  struct Node {
+    int element;
+  };
+  return 0;
+}
