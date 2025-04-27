7a7b7d5d19befddbff078d319950730ffee96694
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-27 13:21:01 +0200

    feat(linked-list): linked-list basic implementation

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -2,13 +2,32 @@
 // Created by Sofiane on 26/04/2025.
 //
 
-// c'est une suite d'élement
-// Chaque element contient une réference (pointeur) a un autre élement
+#include <stddef.h>
+#include <stdio.h>
 
 int main() {
   struct Node {
     int element;
     struct Node *next;
+  };
+
+  struct Node node1;
+  struct Node node2;
+  struct Node node3;
+
+  node1.element = 10;
+  node1.next = &node2;
+
+  node2.element = 20;
+  node2.next = &node3;
+
+  node3.element = 30;
+  node3.next = NULL;
+
+  struct Node *current = &node1;
+  while (current != NULL) {
+    printf("%d\n", current->element);
+    current = current->next;
   };
   return 0;
 }
