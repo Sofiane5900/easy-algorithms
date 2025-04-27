ba4c0c454adb1ad508c1c8c7d4f3d2fb69fd1363
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-27 17:56:28 +0200

    feat(binary-search): adding basic binary search algo

--- binary-search/binary_search.c
+++ binary-search/binary_search.c
@@ -3,11 +3,25 @@
 //
 
 #include <stddef.h>
+#include <stdio.h>
 int main() {
   int number = 5;
-  int numbers[] = {2, 8, 9, 10, 5, 12};
+  int numbers[] = {2, 5, 8, 10, 11, 12};
   size_t count = sizeof(numbers) / sizeof(numbers[0]);
   int left = 0;
+  int right = count - 1;
 
+  while (left <= right) {
+    int middle = (left + right) / 2;
+
+    if (numbers[middle] == number) {
+      printf("Found : %d\n", number);
+      break;
+    } else if (numbers[middle] < number) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
   return 0;
 }
