07d60bea1c8807194cfa3bbdf1760b49b2c304d9
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-27 17:22:27 +0200

    feat(binary-search): adding count

--- binary-search/binary_search.c
+++ binary-search/binary_search.c
@@ -2,9 +2,12 @@
 // Created by Sofiane on 27/04/2025.
 //
 
+#include <stddef.h>
 int main() {
   int number = 5;
-  int left;
-  int right;
+  int numbers[] = {2, 8, 9, 10, 5, 12};
+  size_t count = sizeof(numbers) / sizeof(numbers[0]);
+  int left = 0;
+
   return 0;
 }
