dd8a2daf9a43fb7a774a44dc765b4d97b8f0fe9b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-01 19:04:11 +0200

    feat(array): basic array implemntation

--- array/array.c
+++ array/array.c
@@ -1,9 +1,25 @@
 
-#define include MAX_SIZE 5
+#include <stddef.h>
+#include <stdio.h>
+#define MAX_SIZE 5
+
+struct Array {
+  int data[MAX_SIZE];
+  int size;
+};
 
 int main() {
-  struct Array {
-    int data;
-    int size;
-  };
+
+  struct Array array;
+
+  array.data[0] = 1;
+  array.data[1] = 2;
+  array.data[2] = 3;
+  array.data[3] = 4;
+  array.data[4] = 5;
+
+  array.size = MAX_SIZE;
+  for (int i = 0; i < array.size; i++) {
+    printf("Element of the index [%d]: %d\n", i, array.data[i]);
+  }
 }
