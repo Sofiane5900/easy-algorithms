81434e0f30bbf15f782a7d7591b15cc49b9457c6
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-07 13:41:32 +0200

    feat(selection-sort): loop to find smallestNumber

--- selection-sort/selection-sort.c
+++ selection-sort/selection-sort.c
@@ -3,8 +3,17 @@
 //
 
 #include <stddef.h>
+#include <stdio.h>
 
 int main() {
   int numbers[] = {20, 55, 3, 8, 9, 101};
   int lowestNumber = numbers[0];
+  size_t count = sizeof(numbers) / sizeof(numbers[0]);
+
+  for (int i = 0; i < count; i++) {
+    if (lowestNumber > numbers[i]) {
+      lowestNumber = numbers[i];
+      printf("%d\n", lowestNumber);
+    }
+  }
 }
