c3ec0457ddfc622cd781dfd247c5263be327a99c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-05-07 14:47:48 +0200

    feat(selection-sort): full implementation of sorting algorithm

--- selection-sort/selection-sort.c
+++ selection-sort/selection-sort.c
@@ -7,13 +7,29 @@
 
 int main() {
   int numbers[] = {20, 55, 3, 8, 9, 101};
-  int lowestNumber = numbers[0];
-  int minIndex = 0;
   size_t count = sizeof(numbers) / sizeof(numbers[0]);
 
-  for (int i = 1; i < count; i++) {
-    if (numbers[minIndex] > numbers[i]) {
-      minIndex = i;
+  for (int i = 0; i < count - 1; i++) {
+    int minIndex = i;
+
+    // Find the smallest index
+    for (int j = i + 1; j < count; j++) {
+      if (numbers[minIndex] > numbers[j]) {
+        minIndex = j;
+      }
+    }
+
+    // Use of swap algorithm to switch values
+    if (minIndex != i) {
+      int temp = numbers[i];
+      numbers[i] = numbers[minIndex];
+      numbers[minIndex] = temp;
     }
   }
+
+  // display the values (sorted)
+  for (int i = 0; i < count; i++) {
+    printf("%d ", numbers[i]);
+  }
+  printf("\n");
 }
