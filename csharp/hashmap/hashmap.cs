1a5c781104463cd3c1b6dabc9cd092874cdd3188
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-06 17:30:42 +0200

    feat(hashmap): adding buckets

---
+++ csharp/hashmap/hashmap.cs
@@ -0,0 +1,10 @@
+namespace csharp.hashmap;
+
+public class hashmap
+{
+    // on a besoin d'une liste sur quoi stocker nos indices & élements, 
+    // on a besoin d'une méthode qui va placer les élements avec une clé en tant qu'indice
+
+    
+    private List<KeyValuePair<string, int>>[] buckets = new List<KeyValuePair<string, int>>[];
+}
