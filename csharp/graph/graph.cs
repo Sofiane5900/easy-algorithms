e080c0d08630760a6e32be13ed9a7b6b3c194d37
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-10 16:17:26 +0200

    feat(graph/csharp): adding node class

--- csharp/graph/graph.cs
+++ csharp/graph/graph.cs
@@ -2,5 +2,11 @@ namespace csharp.graph;
 
 public class graph
 {
-    
+    // faire une list de nodes 
+}
+
+public class Node
+{
+    public string Name; 
+    public List<Node> Neighbors { get; set; }
 }
