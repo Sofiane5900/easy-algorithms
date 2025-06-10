dae8591f5752986b5ffaa7e3693655f950faf65f
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-10 16:23:18 +0200

    feat(graph/csharp): adding ctor for node class

--- csharp/graph/graph.cs
+++ csharp/graph/graph.cs
@@ -2,10 +2,17 @@ namespace csharp.graph;
 
 public class Graph
 {
+    
 }
 
 public class Node
 {
     public string Name; 
     public List<Node> Neighbors { get; set; }
+
+    public Node(string name)
+    {
+        Name = name;
+        Neighbors = new List<Node>();
+    }
 }
