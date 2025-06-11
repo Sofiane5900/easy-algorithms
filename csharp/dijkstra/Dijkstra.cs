ab815b6c19830d839bf63d28c943d33321f4468d
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-11 15:34:14 +0200

    feat(dijkstra): adding unvisited list

--- csharp/dijkstra/Dijkstra.cs
+++ csharp/dijkstra/Dijkstra.cs
@@ -10,9 +10,14 @@ public class Dijkstra
     // on boucle pour chaque distances 
     public static void Compute(Graph graph, Node start)
     {
+        var unvisited = new HashSet<Node>(graph.Nodes);
         foreach (var node in graph.Nodes )
         {
             distances[node] = int.MaxValue;
         }
-    }
+        
+        distances[start] = 0;
+        Node current = null;
+        int minDistance = int.MaxValue;
+  
 }
