c82f46858a8f708235b5aac2126ac3e66e24adde
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-11 14:47:57 +0200

    feat(dijkstra): adding a compute method and initlaizing first node to maxvalue

--- csharp/dijkstra/Dijkstra.cs
+++ csharp/dijkstra/Dijkstra.cs
@@ -1,6 +1,18 @@
+using csharp.graph;
+
 namespace csharp.dijkstra;
 
 public class Dijkstra
 {
+    //initailiser mes noeuds et leur distance
+    public static Dictionary<Node, int> distances = new();
     
+    // on boucle pour chaque distances 
+    public static void Compute(Graph graph, Node start)
+    {
+        foreach (var node in graph.Nodes )
+        {
+            distances[node] = int.MaxValue;
+        }
+    }
 }
