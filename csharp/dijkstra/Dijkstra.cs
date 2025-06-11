0f08a951a7c62fe68dc73a132bb7f7525d967bdf
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-11 15:37:48 +0200

    feat(dijkstra): removing the far node

--- csharp/dijkstra/Dijkstra.cs
+++ csharp/dijkstra/Dijkstra.cs
@@ -4,24 +4,22 @@ namespace csharp.dijkstra;
 
 public class Dijkstra
 {
-    //initailiser mes noeuds et leur distance
     public static Dictionary<Node, int> distances = new();
-    
-    // on boucle pour chaque distances 
+
     public static void Compute(Graph graph, Node start)
     {
         var unvisited = new HashSet<Node>(graph.Nodes);
-        foreach (var node in graph.Nodes )
+        foreach (var node in graph.Nodes)
         {
             distances[node] = int.MaxValue;
         }
-        
+
         distances[start] = 0;
         Node current = null;
         int minDistance = int.MaxValue;
         while (unvisited.Count > 0)
         {
-            foreach (var node in unvisited )
+            foreach (var node in unvisited)
             {
                 if (distances[node] < minDistance)
                 {
@@ -29,16 +27,21 @@ public class Dijkstra
                     current = node;
                 }
             }
-            if (current == null) break;
 
-            foreach (var edge in current.Neighbors )
+            if (current == null)
+                break;
+
+            foreach (var edge in current.Neighbors)
             {
                 var neighbor = edge.Target;
                 int alt = distances[neighbor] + edge.Kilometer;
                 if (alt < distances[neighbor])
                 {
                     distances[neighbor] = alt;
-                 }
+                }
             }
+
+            unvisited.Remove(current);
+        }
     }
 }
